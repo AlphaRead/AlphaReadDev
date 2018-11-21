4fdaef9931e866a17613bb9d709b41039e9ee169
MissHead <doskey97>
2018-11-21 19:29:46 -0200

    Questao 2 - Classes Gramaticais

--- app/src/main/java/com/example/allan/appalpharead/AnswerQuestionTwo.java
+++ app/src/main/java/com/example/allan/appalpharead/AnswerQuestionTwo.java
@@ -5,16 +5,33 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.allan.appalpharead.models.DicionarioOnline;
+import com.example.allan.appalpharead.models.DicionarioService;
+import com.example.allan.appalpharead.models.Entry;
+import com.example.allan.appalpharead.models.Sense;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AnswerQuestionTwo extends Activity {
 
     private Button avancar, btnCancel;
     private TextView word;
-
-    private String palavra;
+    private Spinner classlist;
+    private String palavra, selecao, classe;
     private int Point;
 
     private Bundle bundle;
@@ -35,6 +52,7 @@ public class AnswerQuestionTwo extends Activity {
 
         word = findViewById(R.id.word);
         word.setText(palavra);
+
 
         btnCancel = findViewById(R.id.btnCancel);
 
@@ -62,11 +80,14 @@ public class AnswerQuestionTwo extends Activity {
 
             }
         });
-
+        classlist = findViewById(R.id.classlist);
         avancar = findViewById(R.id.btnAvancar);
         avancar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                selecao = String.valueOf(classlist.getSelectedItem());
+                searchInDict(palavra, selecao);
                 Intent it = new Intent(context, AnswerQuestionThree.class);
 
                 it.putExtra("picture", bundle.getString("picture"));
@@ -82,6 +103,98 @@ public class AnswerQuestionTwo extends Activity {
 
                 startActivity(it);
                 finish();
+            }
+        });
+    }
+
+
+    private void searchInDict(final String word, final String selecao){
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://dicionario-aberto.net/") //define a url base
+                .addConverterFactory(GsonConverterFactory.create()) //define o objeto de converção (Gson)
+                .build();
+
+        //retrofit retorna uma classe que implementa DicionarioService(pois este é uma interface e não pode ser instânciada)
+        DicionarioService service = retrofit.create(DicionarioService.class);
+
+        //objeto para fazer a chamada
+        Call<DicionarioOnline> requestDicionario = service.searchWord("search-json/" + word);
+
+        requestDicionario.enqueue(new Callback<DicionarioOnline>() {
+            @Override
+            public void onResponse(Call<DicionarioOnline> call, Response<DicionarioOnline> response) {
+
+                if (response.isSuccessful()) {
+                    try {
+                        //Requisição retornou com sucesso
+                        DicionarioOnline dicionario = response.body();
+                        Entry e = dicionario.entry;
+                        Pattern REMOVE_TAGS = Pattern.compile("<.+?>");
+
+                        for (Sense s : e.sense) {
+                            Matcher m = REMOVE_TAGS.matcher(s.gramGrp);
+                            Log.i("Words", m.replaceAll(";").split(";")[0]);
+                            classe = m.replaceAll(";").split(";")[0];
+                            break;
+                        }
+                        switch(classe){
+                            case "f.":
+                                classe = "Substantivo";
+                                break;
+                            case "m.":
+                                classe = "Substantivo";
+                                break;
+                            case "v. t.":
+                                classe = "Verbo";
+                                break;
+                            case "v. i.":
+                                classe = "Verbo";
+                                break;
+                            case "pron.":
+                                classe = "Pronome";
+                                break;
+                            case "adj.":
+                                classe = "Adjetivo";
+                                break;
+                            case "Art.":
+                                classe = "Artigo";
+                                break;
+                            case "Interj.":
+                                classe = "Interjeição";
+                                break;
+                            case "adv.":
+                                classe = "Advérbio";
+                                break;
+                            case "conj.":
+                                classe = "Conjunção";
+                                break;
+                            case "prep.":
+                                classe = "Preposição";
+                                break;
+                            case "num.":
+                                classe = "Numeral";
+                                break;
+                            default:
+                                Toast.makeText(context, "Classe gramatical não encontrada.", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+
+                        if (classe.equals(selecao)) {
+                            Point += 1;
+                        }
+
+                    }catch (Exception e){
+                        Toast.makeText(context, "Classe gramatical não encontrada.", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DicionarioOnline> call, Throwable t) {
+                Toast.makeText(context, "Palavra '"+word+"' não encontrada.", Toast.LENGTH_LONG).show();
             }
         });
     }
